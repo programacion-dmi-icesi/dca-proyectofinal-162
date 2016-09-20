19162a9a97bd89a7105a84d7988f1da52d178fc7
Jose Moncada <josemoncada87@hotmail.com>
2016-09-20 15:15:53 -0500

    cambio en los estados

--- src/programaciondmi/dca/core/EspecieAbstracta.java
+++ src/programaciondmi/dca/core/EspecieAbstracta.java
@@ -3,10 +3,29 @@ package programaciondmi.dca.core;
 public abstract class EspecieAbstracta {
 	
 	protected int x;
-	protected int y;		
+	protected int y;
+	public final int NORMAL = 0;
+	public final int ENVENENADO = 1;
+	public final int ENFERMO = 2;
+	public final int EXTASIS = 3;
+	public final int MUERTO = 4;
+	private int estado;
 	 
 	public EspecieAbstracta() {
-		// TODO Auto-generated constructor stub
+		estado = NORMAL;
+	}
+	
+	public int getEstado() {
+		return estado;
+	}
+	
+	public void setEstado(int estado) throws Exception {
+		if(estado == NORMAL || estado == ENFERMO || estado == ENVENENADO || estado == EXTASIS|| estado == MUERTO ){
+			this.estado = estado;
+		}else{
+			System.err.println("Estado no valido... use: NORMAL, ENVENENADO, ENFERMO, MUERTO, EXTASIS");		
+			throw new Exception("Ese estado no es valido"); // TODO crear excepcion propia
+		}		
 	}
 	
 	public abstract void dibujar();
--- src/programaciondmi/dca/core/PlantaAbstracta.java
+++ src/programaciondmi/dca/core/PlantaAbstracta.java
@@ -5,7 +5,11 @@ public abstract class PlantaAbstracta {
 	protected int x;
 	protected int y;
 	
+	
 	public PlantaAbstracta(){}
+	
+	
+	
 	
 	public abstract void dibujar();
 
