83116e3f0a9c69530c8ae05984eb4cad01cf2378
AnnieRojas <AnnieRojas@172.30.162.188>
2016-11-09 16:29:08 -0500

    Agregar implement Observer

--- src/programaciondmi/dca/ecosistemas/piedrahitarinconrojasrojas/Mundo.java
+++ src/programaciondmi/dca/ecosistemas/piedrahitarinconrojasrojas/Mundo.java
@@ -1,5 +1,15 @@
 package programaciondmi.dca.ecosistemas.piedrahitarinconrojasrojas;
 
-public class Mundo {
+import java.util.Observable;
+import java.util.Observer;
+
+// crear una clase y heredar de
+public class Mundo implements Observer {
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
