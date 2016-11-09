e6c420c52eb3a61d310cccfa63941a9897cdb799
IvanDario <IvanDario@172.30.175.244>
2016-11-09 16:50:01 -0500

    prueba de pintar planta

--- src/programaciondmi/dca/ecosistemas/erazoecheverryceron/PBuena.java
+++ src/programaciondmi/dca/ecosistemas/erazoecheverryceron/PBuena.java
@@ -1,5 +1,16 @@
 package programaciondmi.dca.ecosistemas.erazoecheverryceron;
 
+import processing.core.PApplet;
+import processing.core.PImage;
+
 public class PBuena extends PlantaPapu {
 
+	private PApplet app;
+	private PImage plantaBuena;
+	
+	public PBuena() {
+	
+		plantaBuena = CargaDatos.PBuena;
+		
+	}
 }
