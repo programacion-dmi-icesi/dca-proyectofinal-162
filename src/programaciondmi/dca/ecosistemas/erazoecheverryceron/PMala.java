1c9697e1c85e49ea0f89e62a9dc9a9557122c2e0
IvanDario <IvanDario@172.30.175.244>
2016-11-09 16:45:04 -0500

    prueba de pintar planta

--- src/programaciondmi/dca/ecosistemas/erazoecheverryceron/PMala.java
+++ src/programaciondmi/dca/ecosistemas/erazoecheverryceron/PMala.java
@@ -1,5 +1,22 @@
 package programaciondmi.dca.ecosistemas.erazoecheverryceron;
 
+import processing.core.PApplet;
+import processing.core.PImage;
+
 public class PMala extends PlantaPapu {
 
+	private PApplet app;
+	private PImage plantaMala;
+	
+	public PMala() {
+
+		
+		plantaMala = CargaDatos.PMala;
+		
+	}
+	
+	public void pintar() {
+		app.image(plantaMala, 50, 50);
+		
+	}
 }
