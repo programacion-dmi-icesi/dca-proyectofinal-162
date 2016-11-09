196b4ca1541872d884c7bb5990d1038f306878b2
Julio César <Julio Csar@172.30.175.101>
2016-11-09 18:34:03 -0500

    data nuevo, cargo imagenes de plantas

--- src/programaciondmi/dca/ecosistemas/erazoecheverryceron/PlantaPapu.java
+++ src/programaciondmi/dca/ecosistemas/erazoecheverryceron/PlantaPapu.java
@@ -2,6 +2,8 @@ package programaciondmi.dca.ecosistemas.erazoecheverryceron;
 
 import java.util.ArrayList;
 
+import processing.core.PApplet;
+import processing.core.PImage;
 import processing.core.PVector;
 import programaciondmi.dca.core.PlantaAbstracta;
 
@@ -10,11 +12,15 @@ public abstract class PlantaPapu extends PlantaAbstracta {
 	protected PVector pos;
 	protected float vida;
 	protected boolean murio;
+	protected PImage plantaBuena, plantaMala;
+	private PApplet app;
 	
 	public PlantaPapu() {
 		pos = new PVector(x,y);
-		
+		plantaBuena = app.loadImage("plantaBuena.png");
+		plantaMala = app.loadImage("plantaMala.png");
 	}
+	
 	
 	@Override
 	public void run() {
