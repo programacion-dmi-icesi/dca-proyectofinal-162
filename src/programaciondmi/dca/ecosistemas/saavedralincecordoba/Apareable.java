64a451ad6bd63000c8a9a53acba2a1df33de167a
Juan Manuel Cordoba <juanmaco-97@hotmail.com>
2016-11-10 08:17:30 -0500

    se crean los metodos en las clases Hervivoro, Canival y Apareable

--- src/programaciondmi/dca/ecosistemas/saavedralincecordoba/Apareable.java
+++ src/programaciondmi/dca/ecosistemas/saavedralincecordoba/Apareable.java
@@ -1,5 +1,44 @@
 package programaciondmi.dca.ecosistemas.saavedralincecordoba;
 
-public class Apareable {
+import programaciondmi.dca.core.EcosistemaAbstracto;
+import programaciondmi.dca.core.EspecieAbstracta;
+import programaciondmi.dca.core.interfaces.IApareable;
+
+public class Apareable extends EspecieAbstracta implements IApareable {
+
+	public Apareable(EcosistemaAbstracto ecosistema) {
+		super(ecosistema);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public EspecieAbstracta aparear(IApareable apareable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dibujar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mover() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean recibirDano(EspecieAbstracta lastimador) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
--- src/programaciondmi/dca/ecosistemas/saavedralincecordoba/Hervivoro.java
+++ src/programaciondmi/dca/ecosistemas/saavedralincecordoba/Hervivoro.java
@@ -1,5 +1,45 @@
 package programaciondmi.dca.ecosistemas.saavedralincecordoba;
 
-public class Hervivoro {
+import programaciondmi.dca.core.EcosistemaAbstracto;
+import programaciondmi.dca.core.EspecieAbstracta;
+import programaciondmi.dca.core.PlantaAbstracta;
+import programaciondmi.dca.core.interfaces.IHerbivoro;
+
+public class Hervivoro extends EspecieAbstracta implements IHerbivoro {
+
+	public Hervivoro(EcosistemaAbstracto ecosistema) {
+		super(ecosistema);
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dibujar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mover() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean recibirDano(EspecieAbstracta lastimador) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void comerPlanta(PlantaAbstracta victima) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
