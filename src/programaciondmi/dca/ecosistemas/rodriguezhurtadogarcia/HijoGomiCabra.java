7b7607e48bd01df4be30246570fc8503e8f6e8be
JUAN DAVID HURTADO R <juanchis.98@hotmail.es>
2016-11-11 14:57:47 -0500

    Se añadio la clase hijo

--- src/programaciondmi/dca/ecosistemas/rodriguezhurtadogarcia/HijoGomiCabra.java
+++ src/programaciondmi/dca/ecosistemas/rodriguezhurtadogarcia/HijoGomiCabra.java
@@ -1,5 +1,37 @@
 package programaciondmi.dca.ecosistemas.rodriguezhurtadogarcia;
 
-public class HijoGomiCabra {
+import programaciondmi.dca.core.EcosistemaAbstracto;
+import programaciondmi.dca.core.EspecieAbstracta;
+
+public class HijoGomiCabra extends EspecieAbstracta {
+
+	public HijoGomiCabra(EcosistemaAbstracto ecosistema) {
+		super(ecosistema);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dibujar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mover() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean recibirDano(EspecieAbstracta lastimador) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
