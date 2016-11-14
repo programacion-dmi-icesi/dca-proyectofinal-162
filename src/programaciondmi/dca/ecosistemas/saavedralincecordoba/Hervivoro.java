69627d36c59eacaf49a86e314af6c628a78694e3
Juan Manuel Cordoba <juanmaco-97@hotmail.com>
2016-11-14 10:29:53 -0500

    Se inicializan algunas variables y crea el hilo del movimiento

--- src/programaciondmi/dca/ecosistemas/saavedralincecordoba/Hervivoro.java
+++ src/programaciondmi/dca/ecosistemas/saavedralincecordoba/Hervivoro.java
@@ -1,21 +1,39 @@
 package programaciondmi.dca.ecosistemas.saavedralincecordoba;
 
+import processing.core.PVector;
+import processing.core.PApplet;
+import processing.core.PImage;
 import programaciondmi.dca.core.EcosistemaAbstracto;
 import programaciondmi.dca.core.EspecieAbstracta;
 import programaciondmi.dca.core.PlantaAbstracta;
 import programaciondmi.dca.core.interfaces.IHerbivoro;
+import programaciondmi.dca.ejecucion.Mundo;
 
 public class Hervivoro extends EspecieAbstracta implements IHerbivoro {
-
+	private int vida;
+	private int velocidad;
+	private PVector dir;
+	private int ciclo;
+	private float fuerza,energia;
+	private PImage personaje;
+private PApplet app;
 	public Hervivoro(EcosistemaAbstracto ecosistema) {
 		super(ecosistema);
-		
+		app=Mundo.ObtenerInstancia().getApp();
 	}
 
 	@Override
+	//SE CREA UN HILO CON LA CONDICION DE QUE MUEVA AL PERSONAJE SOLO SI SU VIDA ES MAYOR A 0
 	public void run() {
 		// TODO Auto-generated method stub
-		
+		while(vida>0){
+			mover();
+			try{
+				
+			}catch(Exception e){
+				
+			}
+		}
 	}
 
 	@Override
