c23dc8417973006b074b9937fba036ed90252e7c
Juan Manuel Cordoba <juanmaco-97@hotmail.com>
2016-11-18 22:00:16 -0500

    se crean variables en la clase PlantaMala

--- src/programaciondmi/dca/ecosistemas/saavedralincecordoba/PlantaMala.java
+++ src/programaciondmi/dca/ecosistemas/saavedralincecordoba/PlantaMala.java
@@ -2,9 +2,20 @@ package programaciondmi.dca.ecosistemas.saavedralincecordoba;
 
 import programaciondmi.dca.core.EspecieAbstracta;
 import programaciondmi.dca.core.PlantaAbstracta;
+import processing.core.PImage;
+import processing.core.PApplet;
+import programaciondmi.dca.ejecucion.Mundo;
 
 public class PlantaMala extends PlantaAbstracta {
-
+	private float posX;
+	private float posY;
+	private PImage plantaM;
+	private PApplet app;
+	
+	public void PlantaMala(){
+		app= Mundo.ObtenerInstancia().getApp();
+	}
+	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
