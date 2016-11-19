a5284e9c24f539024b2cf7665d9aac617b24920e
Juan Manuel Cordoba <juanmaco-97@hotmail.com>
2016-11-18 20:59:44 -0500

    se crean los metodos dentro de la clase

--- src/programaciondmi/dca/ecosistemas/saavedralincecordoba/PlantaBuena.java
+++ src/programaciondmi/dca/ecosistemas/saavedralincecordoba/PlantaBuena.java
@@ -1,5 +1,21 @@
 package programaciondmi.dca.ecosistemas.saavedralincecordoba;
+import processing.core.PImage;
+import programaciondmi.dca.ejecucion.Mundo;
+import processing.core.PApplet;
 
-public class PlantaBuena {
+public class PlantaBuena extends Thread{
+private float posX;
+private float posY;
+private int vida;
+private PImage plantaB;
+private PApplet app;
+
+public void plantaBuena(){
+	app= Mundo.ObtenerInstancia().getApp();
+	plantaB= app.loadImage("");
+}
+public void run(){
+	
+}
 
 }
