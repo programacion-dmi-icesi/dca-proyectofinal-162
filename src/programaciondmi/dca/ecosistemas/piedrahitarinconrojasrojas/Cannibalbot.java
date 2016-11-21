34f69d4a1399cd0606caa7e1fd9e257e95d802f4
Stephany Rojas Gomez <rojasstephany08@gmail.com>
2016-11-21 17:53:53 -0500

    Borré la carpeta de enfermos, porque los cambios los haremos con pixeles. Hice la animación del personaje hoho

--- src/programaciondmi/dca/ecosistemas/piedrahitarinconrojasrojas/Cannibalbot.java
+++ src/programaciondmi/dca/ecosistemas/piedrahitarinconrojasrojas/Cannibalbot.java
@@ -21,101 +21,113 @@ public class Cannibalbot extends EspecieAbstracta implements ICanibal {
 	private PVector dir;
 	PImage[] cannibal = new PImage[20];
 	int posicionpj = 0;
-	int contador=5;
-	int movCannibalbot=0;
+	int contador;
+	int movCannibalbot = 0;
 	private Random random;
+	int vista;
+	int movCanibal;
+
 	public Cannibalbot(EcosistemaAbstracto ecosistema) {
 		super(ecosistema);
 		this.random = new Random();
 		this.x = random.nextInt(Mundo.ObtenerInstancia().getApp().width);
 		this.y = random.nextInt(Mundo.ObtenerInstancia().getApp().height);
 		this.vida = 5;
-		this.velocidad =2;
-		this.energia = 250;
-		this.fuerza = 100;
-		
+		this.velocidad = 3;
+		this.energia = 200;
+		this.fuerza = 90;
+
 		int targetX = random.nextInt();
 		int targetY = random.nextInt();
 		cambiarDireccion(new PVector(targetX, targetY));
 		ciclo = 0;
 		// PERSONAJE ASPECTO
 		PApplet app = Mundo.ObtenerInstancia().getApp();
-		//espalda del magobot
+		// espalda del magobot
 		cannibal[0] = app.loadImage("DataTikiBots/Cannibalbot/cA001.png");
 		cannibal[1] = app.loadImage("DataTikiBots/Cannibalbot/cA002.png");
 		cannibal[2] = app.loadImage("DataTikiBots/Cannibalbot/cA003.png");
 		cannibal[3] = app.loadImage("DataTikiBots/Cannibalbot/cA004.png");
 		cannibal[4] = app.loadImage("DataTikiBots/Cannibalbot/cA005.png");
 
-		//perfil derecho del Cannibalbot
+		// perfil derecho del Cannibalbot
 		cannibal[5] = app.loadImage("DataTikiBots/Cannibalbot/cD001.png");
 		cannibal[6] = app.loadImage("DataTikiBots/Cannibalbot/cD002.png");
 		cannibal[7] = app.loadImage("DataTikiBots/Cannibalbot/cD003.png");
 		cannibal[8] = app.loadImage("DataTikiBots/Cannibalbot/cD004.png");
 		cannibal[9] = app.loadImage("DataTikiBots/Cannibalbot/cD005.png");
 
-		//frente del Cannibalbot
+		// frente del Cannibalbot
 		cannibal[10] = app.loadImage("DataTikiBots/Cannibalbot/cF001.png");
 		cannibal[11] = app.loadImage("DataTikiBots/Cannibalbot/cF002.png");
 		cannibal[12] = app.loadImage("DataTikiBots/Cannibalbot/cF003.png");
 		cannibal[13] = app.loadImage("DataTikiBots/Cannibalbot/cF004.png");
 		cannibal[14] = app.loadImage("DataTikiBots/Cannibalbot/cF005.png");
 
-		//perfil izquierdo del Cannibalbot
+		// perfil izquierdo del Cannibalbot
 		cannibal[15] = app.loadImage("DataTikiBots/Cannibalbot/cZ001.png");
 		cannibal[16] = app.loadImage("DataTikiBots/Cannibalbot/cZ002.png");
 		cannibal[17] = app.loadImage("DataTikiBots/Cannibalbot/cZ003.png");
 		cannibal[18] = app.loadImage("DataTikiBots/Cannibalbot/cZ004.png");
 		cannibal[19] = app.loadImage("DataTikiBots/Cannibalbot/cZ005.png");
 
-		
 		Thread nt = new Thread(this);
 		nt.start();
-		
+
 	}
-	
-
-	
-
 
 	@Override
 	public void comer(EspecieAbstracta victima) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mover() {
 		if (energia > 0) {
-			
 			buscarComida();
-			
-					if (ciclo % 30 == 0) {
-					// Definir una direccion aleatoria cada 3 segundos
-					int targetX = random.nextInt();
-					int targetY = random.nextInt();
-					cambiarDireccion(new PVector(targetX, targetY));
-					System.out.println("CAMBIO DIRECCION!");
-					}
-		
-				
-			// moverse en la dirección asignada actualmente
+
+			if (ciclo % 30 == 0) {
+				// Definir una direccion aleatoria cada 3 segundos
+				// siguiente posicion en X
+				int targetX = random.nextInt();
+				// siguiente posicion en Y
+				int targetY = random.nextInt();
+				cambiarDireccion(new PVector(targetX, targetY));
+				// diagonal derecha arriba
+				if (targetX > this.x && targetY > this.y) {
+					vista = 1;
+					contador = (vista - 1) * 5;
+					// diagonal izquierda abajo
+				} else if (targetX < this.x && targetY < this.y) {
+					vista = 2;
+					contador = (vista - 1) * 5;
+					// diagonal izquierda arriba
+				} else if (targetX > this.x && targetY < this.y) {
+					vista = 3;
+					contador = (vista - 1) * 5;
+					// diagonal derecha abajo
+				} else if (targetX < this.x && targetY > this.y) {
+					vista = 4;
+					contador = (vista - 1) * 5;
+				}
+			}
+
+			// moverse en la direccion asignada
 			this.x += dir.x;
 			this.y += dir.y;
 			energia -= 0.01;
 		}
-		
-		if(this.x > Mundo.ObtenerInstancia().getApp().width || this.x < 0){
-			this.dir.x*=-1;			
+
+		if (this.x > Mundo.ObtenerInstancia().getApp().width || this.x < 0) {
+			this.dir.x *= -1;
 		}
-		if(this.y > Mundo.ObtenerInstancia().getApp().height || this.y < 0){
-			this.dir.y *= -1;			
+		if (this.y > Mundo.ObtenerInstancia().getApp().height || this.y < 0) {
+			this.dir.y *= -1;
 		}
-		
 
 	}
-		
-	@Override
+
 	public void run() {
 		while (vida > 0) {
 			mover();
@@ -127,35 +139,30 @@ public class Cannibalbot extends EspecieAbstracta implements ICanibal {
 			}
 		}
 	}
-	
 
-
-	@Override
 	public void dibujar() {
-				PApplet app = Mundo.ObtenerInstancia().getApp();
-			
-
-		// Image Personaje
-		
-		app.image(cannibal[contador], x, y, 100,100);
+		PApplet app = Mundo.ObtenerInstancia().getApp();
+		if(contador ==5*vista){
+			contador= (vista-1)*5;
+		}
+		// Imagen Personaje
+		app.image(cannibal[contador], x, y,100,100);
 		app.fill(255,0,0);
-		
-	
+		if(app.frameCount%10 == 0){
+			contador++;
+		}
 		
 	}
-	
+
 	private void cambiarDireccion(PVector target) {
 		PVector location = new PVector(x, y);
 		dir = PVector.sub(target, location);
 		dir.normalize();
 		dir.mult(velocidad);
-		//System.out.println("[id=" + id + ", direcion=" + dir + "]");
 	}
-	
-	
-	@Override
+
 	public boolean recibirDano(EspecieAbstracta lastimador) {
-		if(PApplet.dist(x, y, lastimador.getX(), lastimador.getY()) <= (vida/2)){
+		if (PApplet.dist(x, y, lastimador.getX(), lastimador.getY()) <= (vida / 2)) {
 			vida -= 5;
 			try {
 				lastimador.setEstado(EXTASIS);
@@ -164,10 +171,9 @@ public class Cannibalbot extends EspecieAbstracta implements ICanibal {
 			}
 			return true;
 		}
-		
 		return false;
 	}
-	
+
 	private void buscarComida() {
 		List<EspecieAbstracta> todas = Mundo.ObtenerInstancia().getEspecies();
 		for (int i = 0; i < todas.size(); i++) {
