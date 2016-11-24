6d030cea5cb42dec97a6f8f767787d303d200c6e
DanielCorrales <danielfelipecorpor@hotmail.com>
2016-11-24 00:10:02 -0500

    Se crea la clase Logo que hace aparecer el ícono para darle click derecho o izquierdo y aparezcan plantas buenas o malas dependiendo del click presionado, se crea la clase Planta. Se modifica el método Dibujar de la clase EcosistemaDES para colocar las plantas en el lienzo dando click en el logo.

--- src/programaciondmi/dca/ecosistemas/escobarcorralesgrimaldo/EcosistemaDES.java
+++ src/programaciondmi/dca/ecosistemas/escobarcorralesgrimaldo/EcosistemaDES.java
@@ -1,35 +1,51 @@
 package programaciondmi.dca.ecosistemas.escobarcorralesgrimaldo;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
+import processing.core.PApplet;
 import programaciondmi.dca.core.EcosistemaAbstracto;
 import programaciondmi.dca.core.EspecieAbstracta;
 import programaciondmi.dca.core.PlantaAbstracta;
+import programaciondmi.dca.core.interfaces.IHerbivoro;
+import programaciondmi.dca.ecosistemas.sarmientomanzanomoncada.LogoEjemplo;
+import programaciondmi.dca.ejecucion.Mundo;
 
 public class EcosistemaDES extends EcosistemaAbstracto {
-	
+	private PApplet app = Mundo.ObtenerInstancia().getApp();
+	private Logo logoMuf;
+	private Planta planta;
+	private ArrayList<Planta> plantas = new ArrayList<Planta>();
+	private int tipoP = 1, x, y, clasePlanta, vista;
+
 	public EcosistemaDES() {
+
 		super();
+
+		Mundo app2 = Mundo.ObtenerInstancia();
+		logoMuf = new Logo("../data/iconoMuf.svg", this);
+		app2.agregarBoton(logoMuf);
+
 	}
 
 	@Override
 	protected LinkedList<EspecieAbstracta> poblarEspecies() {
 		LinkedList<EspecieAbstracta> especies = new LinkedList<EspecieAbstracta>();
-		
+
 		Carnivoro car = new Carnivoro(this);
 		especies.add(car);
-		
+
 		Omnivoro omn = new Omnivoro(this);
 		especies.add(omn);
 
 		Herbivoro herb = new Herbivoro(this);
 		especies.add(herb);
-		
+
 		Canibal can = new Canibal(this);
 		especies.add(can);
-		
+
 		return especies;
 	}
 
@@ -45,16 +61,15 @@ public class EcosistemaDES extends EcosistemaAbstracto {
 
 		Herbivoro herb = new Herbivoro(this);
 		especies.add(herb);
-		
+
 		Canibal can = new Canibal(this);
 		especies.add(can);
-		
+
 		Carnivoro car = new Carnivoro(this);
 		especies.add(car);
-		
+
 		Omnivoro omn = new Omnivoro(this);
 		especies.add(omn);
-		
 
 		return null;
 	}
@@ -66,14 +81,52 @@ public class EcosistemaDES extends EcosistemaAbstracto {
 
 	@Override
 	public void dibujar() {
+
+		synchronized (plantas) {
+			if (app.dist(logoMuf.getX(), logoMuf.getY(), app.mouseX, app.mouseY) <= 30) {
+				if (Mundo.ObtenerInstancia().getApp().mousePressed == true) {
+					if (app.mouseButton == app.LEFT) {
+						plantas.add(new Planta(0));
+					} else if (app.mouseButton == app.RIGHT) {
+						plantas.add(new Planta(1));
+					}
+				}
+			}
+
+			for (int i = 0; i < plantas.size(); i++) {
+				Planta plant = plantas.get(i);
+				plant.dibujar();
+			}
+
+		}
 		synchronized (especies) {
 			Iterator<EspecieAbstracta> iteradorEspecies = especies.iterator();
-			while(iteradorEspecies.hasNext()){
+			while (iteradorEspecies.hasNext()) {
 				EspecieAbstracta actual = iteradorEspecies.next();
 				actual.dibujar();
 			}
+
+			for (EspecieAbstracta herbi : especies) {
+				for (PlantaAbstracta planti : plantas) {
+
+					if (herbi instanceof IHerbivoro) {
+						float d = app.dist(((Planta) planti).getX(), ((Planta) planti).getY(), herbi.getX(),
+								herbi.getY());
+
+						if (d < 100) {
+
+							planta = (Planta) planti;
+
+							if (vista <= 1) {
+								vista++;
+							}
+
+							planta.setViva(vista);
+
+						}
+					}
+				}
+			}
 		}
 	}
-
 }
-
---
+++ src/programaciondmi/dca/ecosistemas/escobarcorralesgrimaldo/Planta.java
@@ -0,0 +1,126 @@
+//comienza clase Planta
+//importa librerias
+
+package programaciondmi.dca.ecosistemas.escobarcorralesgrimaldo;
+
+import java.util.ArrayList;
+
+import processing.core.PApplet;
+import processing.core.PImage;
+import programaciondmi.dca.core.EspecieAbstracta;
+import programaciondmi.dca.core.PlantaAbstracta;
+import programaciondmi.dca.ejecucion.Mundo;
+
+public class Planta extends PlantaAbstracta {
+
+	// establece variables
+	private int x, y, vida, viva = 0;
+	private boolean muere = false;
+	// arreglo de imágenes
+	private PImage[] plantaB = new PImage[3];
+	private PImage[] plantaM = new PImage[3];
+	private PApplet app;
+	// establece si la planta es buena o mala
+	private int clasePlanta;
+
+	// Comienza constructor de Planta
+	public Planta(int i) {
+
+		this.clasePlanta = i;
+		app = Mundo.ObtenerInstancia().getApp();
+		this.x = (int) app.random(-app.width, app.width);
+		this.y = (int) app.random(-app.height, app.height);
+		this.app = Mundo.ObtenerInstancia().getApp();
+
+		// inicializa el arreglo de imágenes
+		plantaB[0] = app.loadImage("../data/buenatres.png");
+		plantaB[1] = app.loadImage("../data/buenados.png");
+		plantaB[2] = app.loadImage("../data/buenauno.png");
+
+		plantaM[0] = app.loadImage("../data/malatres.png");
+		plantaM[1] = app.loadImage("../data/malados.png");
+		plantaM[2] = app.loadImage("../data/malados.png");
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dibujar() {
+		// TODO Auto-generated method stub
+		// dibuja las plantas en el lienzo
+		PApplet app = Mundo.ObtenerInstancia().getApp();
+
+		switch (clasePlanta) {
+
+		case 0:
+
+			app.image(plantaB[viva], x, y);
+			break;
+
+		case 1:
+
+			app.image(plantaM[viva], x, y);
+			break;
+		}
+	}
+
+	public void ataque() {
+		viva++;
+
+		if (viva > 3) {
+			muere = true;
+		}
+	}
+
+	public boolean isMuere() {
+		return muere;
+	}
+
+	public void setMuere(boolean muere) {
+		this.muere = muere;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getClasePlanta() {
+		return clasePlanta;
+	}
+
+	public void setClasePlanta(int clasePlanta) {
+		this.clasePlanta = clasePlanta;
+	}
+
+	public int getViva() {
+		return viva;
+	}
+
+	public void setViva(int viva) {
+		this.viva = viva;
+	}
+
+	@Override
+	public boolean recibirDano(EspecieAbstracta lastimador) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
