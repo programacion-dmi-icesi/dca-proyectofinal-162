610e6f137578ac6b37e6a9fd40cf42a68f2f4b0c
Stefania Grimaldo <tefagrimaldo@gmail.com>
2016-11-24 07:49:30 -0500

    Cambios movimiento hijo

--- src/programaciondmi/dca/ecosistemas/escobarcorralesgrimaldo/Hijo.java
+++ src/programaciondmi/dca/ecosistemas/escobarcorralesgrimaldo/Hijo.java
@@ -5,13 +5,14 @@ import programaciondmi.dca.core.EcosistemaAbstracto;
 import programaciondmi.dca.core.EspecieAbstracta;
 import programaciondmi.dca.ejecucion.Mundo;
 
-public class Hijo extends EspecieAbstracta{
+public class Hijo extends EspecieAbstracta {
 	private PImage[] hijo = new PImage[4];
 	private int vida;
 	private int velocidad;
 	private PVector dir;
 	private int ciclo;
-	
+	private int ballXDirection = 1, ballYDirection = -1;
+
 	public Hijo(EcosistemaAbstracto ecosistema) {
 		super(ecosistema);
 	}
@@ -41,31 +42,52 @@ public class Hijo extends EspecieAbstracta{
 
 	@Override
 	public void mover() {
-		if (ciclo % 30 == 0) {
-			// Definir una direccion aleatoria cada 3 segundos
-			int targetX = (int) (Math.random() * 500);
-			int targetY = (int) (Math.random() * 500);
-			cambiarDireccion(new PVector(targetX, targetY));
-			//System.out.println("CAMBIO DIRECCION!");
+		if (ciclo % 60 == 0) {
+			PApplet app = Mundo.ObtenerInstancia().getApp();
+
+			x = (int) (x + 10.8 * ballXDirection);
+			y = (int) (y + 8.8 * ballYDirection);
+
+			if (x > app.width - 25 || x < 25) {
+				ballXDirection *= -1;
+			}
+			if (y > app.height - 25 || y < 25) {
+				ballYDirection *= -1;
+			}
+
 		}
-		
-		x+=dir.x;
-		y+=dir.y;
 	}
 
 	@Override
 	public boolean recibirDano(EspecieAbstracta lastimador) {
-		// TODO Auto-generated method stub
+		if (PApplet.dist(x, y, lastimador.getX(), lastimador.getY()) < 100) {
+			vida -= 5;
+			try {
+				if (vida == 20) {
+					lastimador.setEstado(NORMAL);
+				}
+				if (vida == 15) {
+					lastimador.setEstado(ENVENENADO);
+				}
+				if (vida == 10) {
+					lastimador.setEstado(ENFERMO);
+				}
+				if (vida == 5) {
+					lastimador.setEstado(EXTASIS);
+				}
+				if (vida == 0) {
+					lastimador.setEstado(MUERTO);
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				// vida -= 5;
+			}
+			return true;
+
+		}
+
 		return false;
-	}
-	
-	private void cambiarDireccion(PVector target) {
-		PVector location = new PVector(x, y);
-		dir = PVector.sub(target, location);
-		dir.normalize();
-		dir.mult(velocidad);
-		//System.out.println("[id=" + id + ", direcion=" + dir + "]");
 	}
 
 }
-
