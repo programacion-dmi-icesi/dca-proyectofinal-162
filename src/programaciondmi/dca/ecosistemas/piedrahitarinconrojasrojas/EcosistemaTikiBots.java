b76a4a5ded4ed46b8f3db87986f4d8982a116595
alitza <alitza-konata.13@hotmail.com>
2016-11-24 01:31:21 -0500

    se cargan las imagenes de las plantas el mago puede comerlas, muere si se come las plantas malas, tiene los estados normal,enfermo,extasis y muerto al perder toda la barra de energia, descomente el mago del ecosistema tiki

--- src/programaciondmi/dca/ecosistemas/piedrahitarinconrojasrojas/EcosistemaTikiBots.java
+++ src/programaciondmi/dca/ecosistemas/piedrahitarinconrojasrojas/EcosistemaTikiBots.java
@@ -1,6 +1,6 @@
 package programaciondmi.dca.ecosistemas.piedrahitarinconrojasrojas;
 
-import java.util.ArrayList; 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -20,8 +20,7 @@ public class EcosistemaTikiBots extends EcosistemaAbstracto {
 	int tipoPlanta = 0;
 	int cont = 0;
 	private boolean colocar, puedeColocar;
-	
-	
+
 	public EcosistemaTikiBots() {
 		super();
 	}
@@ -30,24 +29,23 @@ public class EcosistemaTikiBots extends EcosistemaAbstracto {
 	public void dibujar() {
 		synchronized (especies) {
 			Iterator<EspecieAbstracta> iteradorEspecies = especies.iterator();
-			while(iteradorEspecies.hasNext()){
+			while (iteradorEspecies.hasNext()) {
 				EspecieAbstracta actual = iteradorEspecies.next();
 				actual.dibujar();
 			}
 		}
-		
+
 		synchronized (plantas) {
 			Iterator<PlantaAbstracta> iteradorPlantas = plantas.iterator();
-			while(iteradorPlantas.hasNext()){
-				//System.out.println("aja.. dibuja");
+			while (iteradorPlantas.hasNext()) {
+				// System.out.println("aja.. dibuja");
 				PlantaAbstracta actual = iteradorPlantas.next();
 				actual.dibujar();
 			}
 		}
-		
-		
-		if(app.frameCount%30==0){
-			puedeColocar=true;
+
+		if (app.frameCount % 30 == 0) {
+			puedeColocar = true;
 		}
 
 		int camX = Mundo.ObtenerInstancia().getCamX();
@@ -58,100 +56,98 @@ public class EcosistemaTikiBots extends EcosistemaAbstracto {
 			if (app.mousePressed == true) {
 
 				if (app.mouseButton == app.LEFT) {
-					
-					PlantaBuena buena = new PlantaBuena(app.mouseX - ((app.width) - camX), (int) (app.mouseY - ((app.height) - camY)));
+
+					PlantaBuena buena = new PlantaBuena(app.mouseX - ((app.width) - camX),
+							(int) (app.mouseY - ((app.height) - camY)));
 					agregarPlanta(buena);
-					puedeColocar=false;
-					
+					puedeColocar = false;
+
 				} else if (app.mouseButton == app.RIGHT) {
-					
-					PlantaMala mala = new PlantaMala(app.mouseX - ((app.width) - camX), (int) (app.mouseY - ((app.height) - camY)));
+
+					PlantaMala mala = new PlantaMala(app.mouseX - ((app.width) - camX),
+							(int) (app.mouseY - ((app.height) - camY)));
 					agregarPlanta(mala);
-					puedeColocar=false;
-					
+					puedeColocar = false;
+
 				}
 			}
 		}
-		
+
 	}
 
 	@Override
 	protected LinkedList<EspecieAbstracta> poblarEspecies() {
 		LinkedList<EspecieAbstracta> especies = new LinkedList<EspecieAbstracta>();
-		
-		/*SaberBot nueva = new SaberBot(this);
-		especies.add(nueva);
-		
-		nueva = new SaberBot(this);
-		especies.add(nueva);
-		
+
+		/*
+		 * SaberBot nueva = new SaberBot(this); especies.add(nueva);
+		 * 
+		 * nueva = new SaberBot(this); especies.add(nueva);
+		 * 
+		 */
+		// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 		Magusbot mago = new Magusbot(this);
 		especies.add(mago);
-		
+
 		mago = new Magusbot(this);
-		especies.add(mago);*/
-		
-		
+		especies.add(mago);
+		// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 		Cannibalbot cani = new Cannibalbot(this);
 		especies.add(cani);
-		
+
 		cani = new Cannibalbot(this);
 		especies.add(cani);
-		
-		if(cont == 0){
+
+		if (cont == 0) {
 			Birdbot ave = new Birdbot(this);
 			especies.add(ave);
 			cont++;
 		}
-		
+
 		return especies;
 	}
 
-
 	@Override
 	protected LinkedList<PlantaAbstracta> poblarPlantas() {
-	LinkedList<PlantaAbstracta> plantas = new LinkedList<PlantaAbstracta>();
-	
+		LinkedList<PlantaAbstracta> plantas = new LinkedList<PlantaAbstracta>();
+
 		return plantas;
 	}
 
-
-
-
 	@Override
 	protected List<EspecieAbstracta> generarIndividuos() {
-		
+
 		// TODO Auto-generated method stub
-		
-		/*SaberBot nueva = new SaberBot(this);
-		especies.add(nueva);*/	
-		
-		/*if(cont == 0){
-		Magusbot mago = new Magusbot(this);
-		especies.add(mago);
-		}*/
-		
+
+		/*
+		 * SaberBot nueva = new SaberBot(this); especies.add(nueva);
+		 */
+
+		// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+		//if (cont == 0) {
+			Magusbot mago = new Magusbot(this);
+			especies.add(mago);
+		//}
+		// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
 		Cannibalbot cani = new Cannibalbot(this);
 		agregarEspecie(cani);
-		
 
 		Birdbot ave = new Birdbot(this);
 		agregarEspecie(ave);
 
-		
 		return especies;
 	}
 
-
 	@Override
 	protected List<PlantaAbstracta> generarPlantas() {
-		//System.out.println("generar plantas");
-	
-		
-		/*PlantaMala mala = new PlantaMala(this);
-		plantas.add(mala);
-		agregarPlanta(mala);*/
-		
+		// System.out.println("generar plantas");
+
+		/*
+		 * PlantaMala mala = new PlantaMala(this); plantas.add(mala);
+		 * agregarPlanta(mala);
+		 */
+
 		return plantas;
 	}
 
--- src/programaciondmi/dca/ecosistemas/piedrahitarinconrojasrojas/Magusbot.java
+++ src/programaciondmi/dca/ecosistemas/piedrahitarinconrojasrojas/Magusbot.java
@@ -1,6 +1,7 @@
 package programaciondmi.dca.ecosistemas.piedrahitarinconrojasrojas;
 
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Random;
 
 import processing.core.PApplet;
@@ -15,17 +16,22 @@ import programaciondmi.dca.ejecucion.Mundo;
 public class Magusbot extends EspecieAbstracta implements IOmnivoro {
 
 	private int vida;
-	private float fuerza;
+	private float fuerza, defensa;
 	private int velocidad;
 	private float energia;
 	private int ciclo;
+	PApplet app = Mundo.ObtenerInstancia().getApp();
 	private PVector dir;
 	PImage[] magobot = new PImage[20];
+	PImage mDefensa, mEnergia, mFuerza, mVelocidad, mPocion;
 	int posicionpj;
 	int contador;
 	int vista;
 	int movMagusBot;
 	private Random random;
+	private boolean animonbuena = false;
+	private boolean animonmala = false;
+	private PlantaAbstracta plantaCerca;
 
 	public Magusbot(EcosistemaAbstracto ecosistema) {
 		super(ecosistema);
@@ -36,6 +42,8 @@ public class Magusbot extends EspecieAbstracta implements IOmnivoro {
 		this.velocidad = 2;
 		this.energia = 250;
 		this.fuerza = 100;
+		this.defensa = 100;
+
 		contador = 0;
 		// this.movMagusBot= (int) (Math.random()*4+1);
 
@@ -71,6 +79,12 @@ public class Magusbot extends EspecieAbstracta implements IOmnivoro {
 		magobot[18] = app.loadImage("DataTikiBots/Magusbot/mZ04.png");
 		magobot[19] = app.loadImage("DataTikiBots/Magusbot/mZ05.png");
 
+		mPocion = app.loadImage("DataTikiBots/iconos/envenenado.png");
+		mVelocidad = app.loadImage("DataTikiBots/iconos/iconoVelocidad.png");
+		mEnergia = app.loadImage("DataTikiBots/iconos/iconoEnergia.png");
+		mFuerza = app.loadImage("DataTikiBots/iconos/iconoFuerza.png");
+		mDefensa = app.loadImage("DataTikiBots/iconos/iconoDefensa.png");
+
 		Thread nt = new Thread(this);
 		nt.start();
 
@@ -82,29 +96,171 @@ public class Magusbot extends EspecieAbstracta implements IOmnivoro {
 
 	}
 
-	@Override
-	public void comerPlanta(PlantaAbstracta victima) {
-		// TODO Auto-generated method stub
+	public void dibujar() {// pintar
+
+		// animaciones encima de la barra superior (iconos)
+
+		if (animonbuena == true) {
+			app.fill(0);
+			app.text("+20", x, y - 70);
+			app.text("+20", x + 30, y - 70);
+			app.text("+0.05", x + 60, y - 70);
+			app.image(mFuerza, x, y - 60, 30, 30);
+			app.image(mDefensa, x + 30, y - 60, 30, 30);
+			app.image(mVelocidad, x + 60, y - 60, 30, 30);
+
+		}
+		if (animonmala == true) {
+			app.fill(0);
+			app.text("-20", x, y - 70);
+			app.text("-20", x + 30, y - 70);
+			app.text("-0.01", x + 60, y - 70);
+			app.image(mFuerza, x, y - 60, 30, 30);
+			app.image(mDefensa, x + 30, y - 60, 30, 30);
+			app.image(mVelocidad, x + 60, y - 60, 30, 30);
+
+		}
+
+		app.noStroke();
+		switch (vida) {
+
+		case 100:
+
+			app.fill(0, 255, 0);
+			app.rect(x + 05, y - 20, 20, 10);
+			app.rect(x + 25, y - 20, 20, 10);
+			app.rect(x + 45, y - 20, 20, 10);
+			app.rect(x + 65, y - 20, 20, 10);
+			app.rect(x + 85, y - 20, 20, 10);
+			break;
+		case 80:
+			app.fill(255, 255, 0);
+			app.rect(x + 05, y - 20, 20, 10);
+			app.rect(x + 25, y - 20, 20, 10);
+			app.rect(x + 45, y - 20, 20, 10);
+			app.rect(x + 65, y - 20, 20, 10);
+			break;
+		case 60:
+			app.fill(255, 0, 0);
+			app.rect(x + 05, y - 20, 20, 10);
+			app.rect(x + 25, y - 20, 20, 10);
+			app.rect(x + 45, y - 20, 20, 10);
+			break;
+		case 40:
+			app.fill(255, 0, 255);
+			app.rect(x + 05, y - 20, 20, 10);
+			app.rect(x + 25, y - 20, 20, 10);
+			break;
+		case 20:
+			app.fill(255, 0, 255);
+			app.rect(x + 05, y - 20, 20, 10);
+			break;
+		case 0:
+
+			break;
+		}
+
+		if (getEstado() == NORMAL) {
+
+			app.noTint();
+		}
+		if (getEstado() == ENFERMO) {
+
+			app.tint(0, 255, 0);
+		}
+
+		if (getEstado() == ENVENENADO) {
+
+			app.tint(156, 28, 247);
+		}
+
+		if (getEstado() == EXTASIS) {
+			app.tint(28, 45, 247);
+		}
+
+		if (getEstado() == MUERTO) {
+			app.image(mPocion, x + 25, y - 60, 50, 50);
+			app.tint(63, 63, 63);
+
+		}
+
+		PApplet app = Mundo.ObtenerInstancia().getApp();
+		// Image Personaje
+
+		if (contador == 5 * vista) {
+			contador = (vista - 1) * 5;
+		}
+
+		// System.out.println(magobot[contador]);
+
+		app.image(magobot[contador], x, y, 100, 100);
+		app.fill(255, 0, 0);
+
+		if (app.frameCount % 10 == 0) {
+			contador++;
+		}
+
+		app.noStroke();
+		switch (vida) {
+
+		case 100:
+			app.fill(0, 255, 0);
+			app.rect(x + 05, y - 20, 20, 10);
+			app.rect(x + 25, y - 20, 20, 10);
+			app.rect(x + 45, y - 20, 20, 10);
+			app.rect(x + 65, y - 20, 20, 10);
+			app.rect(x + 85, y - 20, 20, 10);
+			break;
+		case 80:
+			app.fill(255, 255, 0);
+			app.rect(x + 05, y - 20, 20, 10);
+			app.rect(x + 25, y - 20, 20, 10);
+			app.rect(x + 45, y - 20, 20, 10);
+			app.rect(x + 65, y - 20, 20, 10);
+			break;
+		case 60:
+			app.fill(255, 0, 0);
+			app.rect(x + 05, y - 20, 20, 10);
+			app.rect(x + 25, y - 20, 20, 10);
+			app.rect(x + 45, y - 20, 20, 10);
+			break;
+		case 40:
+			app.fill(255, 0, 255);
+			app.rect(x + 05, y - 20, 20, 10);
+			app.rect(x + 25, y - 20, 20, 10);
+			break;
+		case 20:
+			app.fill(255, 0, 255);
+			app.rect(x + 05, y - 20, 20, 10);
+			break;
+		case 0:
+
+			break;
+		}
 
 	}
 
-	@Override
 	public void mover() {
 		// TODO Auto-generated method stub
 		if (energia > 0) {
 
-			//buscarComida();
+			// buscarComida();
+		} else {
+			buscarComida();
 
-			if (ciclo % 30 == 0) {
+			if (plantaCerca != null) {
+				comerPlanta(plantaCerca);
+			}
+
+			if (ciclo % 30 == 0 && plantaCerca == null) {
 				// Definir una direccion aleatoria cada 5 segundos
 				// Siguiente posici�n en x
 				int targetX = random.nextInt();
 				// Siguiente posici�n en y
 				int targetY = random.nextInt();
 				cambiarDireccion(new PVector(targetX, targetY));
-				//System.out.println(x+" "+y);
-				//System.out.println("CAMBIO DIRECCION!");
-				/*if (targetX > this.x && targetY > this.y) {// derechaarriba
+				// System.out.println(x+" "+y);
+				if (targetX > this.x && targetY > this.y) {// derechaarriba
 					vista = 1;
 					contador = (vista - 1) * 5;
 				} else if (targetX < this.x && targetY < this.y) {// izquierdaAbajo
@@ -116,30 +272,17 @@ public class Magusbot extends EspecieAbstracta implements IOmnivoro {
 				} else if (targetX > this.x && targetY < this.y) {// derechaAbajo
 					vista = 4;
 					contador = (vista - 1) * 5;
-				}*/
+				}
 			}
 
-			// moverse en la direcciÃ³n asignada actualmente
+			// moverse en la direcci�n asignada actualmente
 			this.x += dir.x;
 			this.y += dir.y;
 			energia -= 0.01;
 		}
 
-		// if(this.x > Mundo.ObtenerInstancia().getApp().width || this.x < 0){
-		// this.dir.x*=-1;
-		// }
-		// if(this.y > Mundo.ObtenerInstancia().getApp().height || this.y < 0){
-		// this.dir.y *= -1;
-		// }
-		// if(this.x < Mundo.ObtenerInstancia().getApp().width || this.x > 0){
-		// this.dir.x*=+1;
-		// }
-		// if(this.y < Mundo.ObtenerInstancia().getApp().height || this.y > 0){
-		// this.dir.y *= +1;
-		// }
 	}
 
-	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		while (vida > 0) {
@@ -153,63 +296,46 @@ public class Magusbot extends EspecieAbstracta implements IOmnivoro {
 		}
 	}
 
-	@Override
-	public void dibujar() {// pintar
-	
-		PApplet app = Mundo.ObtenerInstancia().getApp();
-		app.background(255);
-		// Image Personaje
+	private void buscarComida() {
+		synchronized (this.ecosistema.getPlantas()) {
+			List<PlantaAbstracta> all = Mundo.ObtenerInstancia().getPlantas();
+			ListIterator<PlantaAbstracta> iterador = all.listIterator();
+			boolean encontro = false;
+			// System.out.println(all.size());
 
-		/*if (contador == 5 * vista) {
-			contador = (vista - 1) * 5;
-		}*/
-		//System.out.println(magobot[contador]);
-		
-		app.image(magobot[contador], x, y, 100, 100);
-		app.fill(255, 0, 0);
-		/*if (app.frameCount % 10 == 0) {
-			contador++;
-		}*/
-		
-		
-		app.noStroke();
-		switch (vida) {
-		
-		case 100:
-			app.fill(0,255,0);
-			app.rect(x+05, y-20, 20, 10);
-			app.rect(x+25, y-20, 20, 10);
-			app.rect(x+45, y-20, 20, 10);
-			app.rect(x+65, y-20, 20, 10);
-			app.rect(x+85, y-20, 20, 10);
-		break;
-		case 80:
-			app.fill(255,255,0);
-			app.rect(x+05, y-20, 20, 10);
-			app.rect(x+25, y-20, 20, 10);
-			app.rect(x+45, y-20, 20, 10);
-			app.rect(x+65, y-20, 20, 10);
-			break;
-		case 60:
-			app.fill(255,0,0);
-			app.rect(x+05, y-20, 20, 10);
-			app.rect(x+25, y-20, 20, 10);
-			app.rect(x+45, y-20, 20, 10);
-			break;
-		case 40:
-			app.fill(255,0,255);
-			app.rect(x+05, y-20, 20, 10);
-			app.rect(x+25, y-20, 20, 10);
-			break;
-		case 20:
-			app.fill(255,0,255);
-			app.rect(x+05, y-20, 20, 10);
-			break;
-		case 0:
-			
-			break;
+			while (iterador.hasNext()) {
+				PlantaAbstracta planta = iterador.next();
+				if (planta instanceof PlantaMala) {
+					PlantaMala pmala = (PlantaMala) planta;
+					float distancia = app.dist(x, y, pmala.getX(), pmala.getY());
+					if (distancia < energia) {
+						encontro = true;
+						plantaCerca = planta;
+						cambiarDireccion(new PVector(pmala.getX(), pmala.getY()));
+
+					}
+				} else if (planta instanceof PlantaBuena) {
+					PlantaBuena pbuena = (PlantaBuena) planta;
+					float distancia = app.dist(x, y, pbuena.getX(), pbuena.getY());
+					if (distancia < energia) {
+						encontro = true;
+						plantaCerca = planta;
+						cambiarDireccion(new PVector(pbuena.getX(), pbuena.getY()));
+
+					}
+				}
+
+			}
+
+			if (!encontro) {
+				plantaCerca = null;
+			}
 		}
-
+		// List<EspecieAbstracta> todas =
+		// Mundo.ObtenerInstancia().getEspecies();
+		// for (int i = 0; i < todas.size(); i++) {
+		// comer(todas.get(i));
+		// }
 	}
 
 	private void cambiarDireccion(PVector target) {
@@ -220,7 +346,11 @@ public class Magusbot extends EspecieAbstracta implements IOmnivoro {
 		// System.out.println("[id=" + id + ", direcion=" + dir + "]");
 	}
 
-	@Override
+	public String toString() {
+		return "EspecieBlanca [id=" + id + ", vida=" + vida + ", fuerza=" + fuerza + ", parejaCercana=" + ", dir=" + dir
+				+ ", x=" + x + ", y=" + y + ", estado=" + estado + "]";
+	}
+
 	public boolean recibirDano(EspecieAbstracta lastimador) {
 		// TODO Auto-generated method stub
 		if (PApplet.dist(x, y, lastimador.getX(), lastimador.getY()) <= (vida / 2)) {
@@ -235,10 +365,78 @@ public class Magusbot extends EspecieAbstracta implements IOmnivoro {
 		return false;
 	}
 
-	private void buscarComida() {
-		List<EspecieAbstracta> todas = Mundo.ObtenerInstancia().getEspecies();
-		for (int i = 0; i < todas.size(); i++) {
-			comer(todas.get(i));
+	public void comerPlanta(PlantaAbstracta victima) {
+		// TODO Auto-generated method stub
+		if (victima.recibirDano(this)) {
+			try {
+				if (victima.getClass() == PlantaMala.class) {
+					PlantaMala m = (PlantaMala) victima;
+					fuerza -= 20;
+					defensa -= 20;
+					vida -= 20;
+					velocidad -= 0.01;
+
+					animonmala = true;
+					animonbuena = false;
+
+					switch (vida) {
+					case 100:
+						setEstado(NORMAL);
+
+						break;
+					case 80:
+						setEstado(ENFERMO);
+						break;
+					case 60:
+						setEstado(EXTASIS);// antes de morir
+						break;
+					case 40:
+						setEstado(MUERTO);
+						animonbuena = false;
+						animonmala = false;
+						break;
+					}
+				}
+
+				if (victima.getClass() == PlantaBuena.class) {
+					PlantaBuena m = (PlantaBuena) victima;
+
+					animonbuena = true;
+					animonmala = false;
+					fuerza += 20;
+					defensa += 20;
+					velocidad += 0.05;
+
+					if (vida <= 80) {
+						vida += 20;
+					}
+
+					switch (vida) {
+					case 100:
+						setEstado(NORMAL);
+
+						break;
+					case 80:
+						setEstado(ENFERMO);
+						break;
+					case 60:
+						setEstado(EXTASIS);
+						break;
+					case 40:
+						setEstado(MUERTO);
+						break;
+					}
+				}
+
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			Mundo.ObtenerInstancia().getPlantas().remove(victima);
+			this.ecosistema.getPlantas().remove(victima);
+			energia += 5;
 		}
 	}
+
 }
